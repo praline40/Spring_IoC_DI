04b37edccbda80755563740edb8f02fd6a1e2c34
Jules <praline40@hotmail.fr>
2020-06-18 15:54:17 -0400

    aa

---
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -0,0 +1,65 @@
+package com.wildcodeschool.wildandwizard.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.wildcodeschool.wildandwizard.entity.Wizard;
+import com.wildcodeschool.wildandwizard.repository.WizardDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WizardController {
+     
+    //private WizardRepository repository = new WizardRepository();
+    //private WizardDao repository = new WizardRepository();
+    //private WizardDao repository = new WizardDao(); // erreur, on implémente pas une interface
+    @Autowired // demande à Spring de créer une instance de WizardRepository, UNIQUE implémentation de l'interface WizardDao
+    private WizardDao repository;
+
+    @GetMapping("/wizards")
+    public String getAll(Model model) {
+        model.addAttribute("wizards", repository.findAll());
+
+        return "wizards";
+    }
+
+    @GetMapping("/wizard")
+    public String getWizard(Model model,
+                            @RequestParam(required = false) Long id) {
+
+        Wizard wizard = new Wizard();
+        if (id != null) {
+            wizard = repository.findById(id);
+        }
+        model.addAttribute("wizard", wizard);
+
+        return "wizard";
+    }
+
+    @PostMapping("/wizard")
+    public String postWizard(@ModelAttribute Wizard wizard) {
+
+        if (wizard.getId() != null) {
+            repository.update(wizard);
+        } else {
+            repository.save(wizard);
+        }
+        return "redirect:/wizards";
+    }
+
+    @GetMapping("/wizard/delete")
+    public String deleteWizard(@RequestParam Long id) {
+
+        repository.deleteById(id);
+
+        return "redirect:/wizards";
+    }
+
+    @GetMapping("/")
+    public String index() {
+
+        return "redirect:/wizards";
+    }
+}
